ade738743474514eab0aec12c7b433d051371bc6
lugarbaver <lugarbaver887@gmail.com>
2025-08-07 15:35:37 +0900

    Update and rename lzpc2.js to lz-pc2.js

--- lz-pc2.js
+++ lz-pc2.js
@@ -1,4 +1,3 @@
-<script>
 document.getElementById('jingshi').innerHTML = '温馨提示：卡顿加载，请清理缓存，关闭手机屏幕，刷新网页，重新登录等均为常规现象，请耐心等待';
 document.getElementById('pcode').innerHTML = '手机下载请扫码获取';
 document.getElementById('n_foot').innerHTML = '<div class="n_copy">版权声明：请勿非法使用储存分享、存储、传播等涉黄内容。</div>&copy; 2025 Alipan';
@@ -13,4 +12,3 @@ var qrcode = new QRCode('code', {
     colorLight : '#ffffff',
     correctLevel : QRCode.CorrectLevel.H
 });
-</script>
