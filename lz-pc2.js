97e6dd9c6c2ea8b3e377308e918e7304a40863d1
lugarbaver <lugarbaver887@gmail.com>
2025-08-07 15:44:21 +0900

    Update lz-pc2.js

--- lz-pc2.js
+++ lz-pc2.js
@@ -1,4 +1,4 @@
-document.getElementById('jingshi').innerHTML = '温馨提示：卡顿加载，请清理缓存，关闭手机屏幕，刷新网页，重新登录等均为常规现象，请耐心等待';
+document.getElementById('jingshi').innerHTML = '反诈提示：刷单兼职，冒充公检法，共享手机屏幕，裸聊敲诈，赌博等都是诈骗，请立即举报';
 document.getElementById('pcode').innerHTML = '手机下载请扫码获取';
 document.getElementById('n_foot').innerHTML = '<div class="n_copy">版权声明：请勿非法使用储存分享、存储、传播等涉黄内容。</div>&copy; 2025 Alipan';
 document.getElementById('rpt').innerHTML = '举报';
